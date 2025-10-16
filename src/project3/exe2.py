301995cf8ce5bf8360a1621c063a7406d94a7f1e
probuborka <probuborka@gmail.com>
2025-10-16 23:51:15 +0300

    fix: exe2.py

--- src/project3/exe2.py
+++ src/project3/exe2.py
@@ -1,24 +1,12 @@
-def swap(list_1, list_2):
-    print(f"до    {list_1}{list_2}")
+def swap(list1, list2):
+    print(f"до    {list1}{list2}")
 
-    list_max = list_1
-    list_min = list_2
-    if len(list_1) < len(list_2):
-        list_max = list_2
-        list_min = list_1
+    len1 = len(list1)
+    list1.extend(list2)
+    list2[:] = list1[:len1]
+    del list1[:len1]
 
-    min_len = len(list_min)
-
-    for i in range(len(list_max)):
-        tmp = list_max[i]
-        if min_len > i:
-            list_max[i] = list_min[i]
-            list_min[i] = tmp
-        else:
-            list_min.append(tmp)
-
-    del list_max[min_len:]
-    print(f"после {list_1}{list_2}")
+    print(f"после {list1}{list2}")
 
 swap([1, 2, 3], [1, 2, 3, 7, 4, 3])
 swap([1, 2, 3], [4, 5, 6])
