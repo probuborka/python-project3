4bc1e299f1aea3cf5cbd7f0a6b27188a04663e37
probuborka <probuborka@gmail.com>
2025-10-17 08:02:39 +0300

    fix: exe2.py

--- src/project3/exe2.py
+++ src/project3/exe2.py
@@ -1,14 +1,19 @@
-def swap(list1, list2):
-    print(f"до    {list1}{list2}")
+def print_result(func):
+    def wrapper(*args, **kwargs):
+        print(f"до    {args}")
+        result = func(*args, **kwargs)
+        print(f"после {args}")
+        return result
+    return wrapper
 
+def swap(list1, list2):
     len1 = len(list1)
     list1.extend(list2)
     list2[:] = list1[:len1]
     del list1[:len1]
 
-    print(f"после {list1}{list2}")
-
-swap([], [])
-swap([1, 2, 3], [1, 2, 3, 7, 4, 3])
-swap([1, 2, 3], [4, 5, 6])
+p = print_result(swap)
+p([], [])
+p([1, 2, 3], [1, 2, 3, 7, 4, 3])
+p([1, 2, 3], [4, 5, 6])
 
